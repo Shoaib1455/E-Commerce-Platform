3038f6672226cb65711a1dcb57707cb5d18629db
Muhammad Shoaib <muhammad.shoaib>
2026-02-12 17:56:13 +0500

    images-added-correctly

---
+++ E-commerce.ViewModels/ProductImagesVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.ViewModels
+{
+    public class ProductImagesVM
+    {
+        public int? Productid { get; set; }
+
+        public string Imageurl { get; set; } = null!;
+
+        public bool Isprimary { get; set; }
+
+        public DateTimeOffset? Createdat { get; set; }
+    }
+}
